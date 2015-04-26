992b2204687c9ec5b0498692a05fee38c003383f
sumotoy <sumotoy@gmail.com>
2015-04-26 02:26:04 +0200

    working beta

---
+++ FT5206.cpp
@@ -0,0 +1,143 @@
+/************************************************************************
+This is a library to handling capacitive multitouch sensors using FT5206.
+Originally written to work with ER-TFTM070-5 (LCD module) from EastRising.
+
+Written by Helge Langehaug, February 2014
+
+BSD license, all text above must be included in any redistribution
+-------------------------------------------------------------------------------
+Modified by Max MC Costa from .s.u.m.o.t.o.y. to correctly return coordinates,
+fixed initialization, added gesture, touch state and faster operations.
+*************************************************************************/
+
+#include <SPI.h>
+#include "Wire.h"
+#include "FT5206.h"
+
+
+static volatile boolean _touched = false;
+
+
+FT5206::FT5206(uint8_t CTP_INT)
+{
+    _ctpInt = CTP_INT;
+	_maxTouch = 5;
+}
+
+
+void FT5206::isr(void)
+{
+	_touched = true;
+}
+
+void FT5206::begin(void) 
+{
+    pinMode(_ctpInt ,INPUT);
+    Wire.begin();
+	#if ARDUINO >= 157
+		Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+	#else
+		TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+	#endif
+	delay(10);
+	writeRegister(FT5206_DEVICE_MODE,0);
+#ifdef digitalPinToInterrupt
+    attachInterrupt(digitalPinToInterrupt(_ctpInt),isr,FALLING);
+#else
+    attachInterrupt(0,isr,FALLING);
+#endif
+}
+  
+
+bool FT5206::touched()
+{
+    if (_touched){
+		_touched = false;
+		return true;
+    }
+	return false;
+}
+
+void FT5206::writeRegister(uint8_t reg,uint8_t val)
+{
+    Wire.beginTransmission(FT5206_I2C_ADDRESS);
+    Wire.write(reg);
+    Wire.write(val);
+    Wire.endTransmission(FT5206_I2C_ADDRESS);
+}
+ 
+ 
+void FT5206::setTouchLimit(uint8_t limit)
+{
+	if (limit > 5) limit = 5;
+	_maxTouch = limit;
+}
+
+uint8_t FT5206::getTouchLimit(void)
+{
+	return _maxTouch;
+}
+
+uint8_t FT5206::getTScoordinates(uint16_t (*touch_coordinates)[2], uint8_t *reg)
+{
+	uint8_t touches = reg[FT5206_TD_STATUS] & 0xF;
+	uint8_t i;
+	if (touches < 1) return 0;
+ 	for (i=1;i<=touches;i++){
+		touch_coordinates[i-1][0] = ((reg[coordRegStart[i-1]] & 0x0f) << 8) | reg[coordRegStart[i-1] + 1];
+		touch_coordinates[i-1][1] = ((reg[coordRegStart[i-1] + 2] & 0x0f) << 8) | reg[coordRegStart[i-1] + 3];
+		if (i == _maxTouch) return i;
+	} 
+    return touches;
+}
+
+void FT5206::getTSregisters(uint8_t *registers) 
+{
+    Wire.requestFrom(FT5206_I2C_ADDRESS, FT5206_REGISTERS); 
+    uint8_t register_number = 0;
+    while(Wire.available()) {
+      registers[register_number++] = Wire.read();
+    }
+}
+  
+
+uint8_t FT5206::getGesture(uint8_t *reg) 
+{
+	if (_maxTouch < 2) return 0;
+	uint8_t temp = reg[FT5206_GEST_ID];
+	switch(temp){
+		case 0x00: //not rec
+			return 0;
+		break;
+		case 0x10: //move up
+			return 1;
+		break;
+		case 0x14: //move left
+			return 2;
+		break;
+		case 0x18: //move down
+			return 3;
+		break;
+		case 0x1C: //move right
+			return 4;
+		break;
+		case 0x48: //zoom in
+			return 5;
+		break;
+		case 0x49: //zoom out
+			return 6;
+		break;
+		default:
+		return temp;
+	}
+	return 0;
+}
+
+uint8_t FT5206::getTSflag(uint8_t *reg) 
+{
+	uint8_t temp = reg[FT5206_TOUCH1_XH];
+	//if (!bitRead(temp,7) && !bitRead(temp,6)) return 3;//
+	if (!bitRead(temp,7) && bitRead(temp,6)) return 1;//finger up
+	if (bitRead(temp,7) && !bitRead(temp,6)) return 2;//finger down
+	return 0;
+}
---
+++ FT5206.h
@@ -0,0 +1,87 @@
+/************************************************************************
+This is a library to handling capacitive multitouch sensors using FT5206.
+Originally written to work with ER-TFTM070-5 (LCD module) from EastRising.
+
+Written by Helge Langehaug, February 2014
+
+BSD license, all text above must be included in any redistribution
+-------------------------------------------------------------------------------
+Modified by Max MC Costa from .s.u.m.o.t.o.y. to correctly return coordinates,
+fixed initialization, added gesture, touch state and faster operations.
+*************************************************************************/
+
+#ifndef _FT5206_H_
+#define _FT5206_H_
+
+#if defined(ENERGIA) // LaunchPad, FraunchPad and StellarPad specific
+	#include "Energia.h"
+#else
+	#include "Arduino.h"
+#endif
+
+/* FT5206 definitions */
+#define FT5206_I2C_ADDRESS 			0x38
+#define FT5206_REGISTERS 	31     // there are more registers, but this is enought to get all 5 touch coordinates.                    
+#define FT5206_TOUCH1_XH 			0x03
+#define FT5206_GEST_ID 				0x01
+#define FT5206_DEVICE_MODE 			0x00
+//number of touched points [3:0] , valid 1..5
+#define FT5206_TD_STATUS 			0x02//n. of touch points [R]
+/*
+bit[7:0] the value will be x4 times the register value (280)
+*/
+#define FT5206_ID_G_THGROUP 		0x80//valid touching detect threshold [R/W]
+/*
+bit[7:0]  (60)
+*/
+#define FT5206_ID_G_THPEAK 			0x81//valid touching peak detect threshold [R/W]
+/*
+bit[7:0]  (16)
+*/
+#define FT5206_ID_G_THCAL 			0x82//the threshold when calculating the focus of touching[R/W]
+/*
+bit[7:0] value it's x32 times the register val (20)
+*/
+#define FT5206_ID_G_THDIFF	 		0x85//the threshold where the coordinatis differs from orig[R/W]
+#define FT5206_ID_G_CLTR	 		0x86//power control mode[R/W]
+
+/*
+TOUCH - XH
+bit[7:6] event flag -> 00:put down
+					-> 01:put up
+					-> 10:contact
+					-> 11: (reserved)
+bit[5:4] (reserved)
+bit[3:0] touch MSB X pos (pix)
+TOUCH - XL
+bit[7:0] touch LSB X pos (pix)
+TOUCH - YH
+bit[7:4] touch ID of Touch Point
+bit[3:0] touch MSB Y pos (pix)
+TOUCH - YL
+bit[7:0] touch LSB Y pos (pix)
+*/
+
+
+
+class FT5206 {
+ public:
+	FT5206(uint8_t CTP_INT);
+	void 				begin(void);
+	uint8_t 			getTScoordinates(uint16_t (*touch_coordinates)[2], uint8_t *reg);
+	void 				getTSregisters(uint8_t *registers);
+	bool 				touched();
+	uint8_t 			getGesture(uint8_t *reg);
+	uint8_t 			getTSflag(uint8_t *reg);
+	void 				writeRegister(uint8_t reg,uint8_t val);
+	void 				setTouchLimit(uint8_t limit);
+	uint8_t 			getTouchLimit(void);
+	
+ private:
+	static void 		 isr(void);
+	uint8_t 			_ctpInt;
+	uint8_t				_maxTouch;
+	const uint8_t coordRegStart[5] = {{0x03},{0x09},{0x0F},{0x15},{0x1B}};
+};
+
+#endif
